827710621550c7ef52f23413f3495448d522174f
Abin Joseph <73039692+abjs@users.noreply.github.com>
2021-01-03 11:35:18 +0530

    Update config.php

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "hms";
+$mysql_hostname = "sql107.epizy.com";
+$mysql_user = "epiz_27598123";
+$mysql_password = "warMO1SXSnZ2";
+$mysql_database = "epiz_27598123_hms";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
 
